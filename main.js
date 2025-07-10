81d57c79939f357d5037b66e376dde0f67cf7981
parham khoram <khoramparham@gmail.com>
2025-07-10 18:25:20 +0330

    setup swagger

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+const swaggerConfig = require("./src/config/swagger.config");
 async function main() {
   const app = express();
   const port = process.env.PORT;
   require("./src/config/mongoose.config");
+  swaggerConfig(app);
   app.listen(port, () => {
-    console.log("app run successfully");
+    console.log(`app run successfully on port ${port}`);
   });
 }
 main();
---
+++ src/config/swagger.config.js
@@ -0,0 +1,18 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+function swaggerConfig(app) {
+  const openapiSpecification = swaggerJsdoc({
+    swaggerDefinition: {
+      openapi: "3.0.0",
+      info: {
+        title: "assignment management system",
+        description: "",
+        version: "1.0.0",
+      },
+    },
+    apis: ["./src/routes*.js"],
+  });
+  const swagger = swaggerUi.setup(openapiSpecification, {});
+  app.use("/", swaggerUi.serve, swagger);
+}
+module.exports = swaggerConfig;
